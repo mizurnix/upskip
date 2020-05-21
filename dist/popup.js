bec37803907af1881c0c64863d1210582aa868c3
mizurnix <mezurnishvili.giorgi@gmail.com>
2020-05-21 17:34:38 +0400

    - Add support for max rate - Add eslint

--- dist/popup.js
+++ dist/popup.js
@@ -1,80 +1,95 @@
-"use strict";
+/* eslint-disable no-undef */
+'use strict'
 
-const form = document.forms["skiplist"];
-const countriesInput = form.elements["countries"];
-const jobTierCheckboxes = form.elements["job-tier"];
+const form = document.forms.skiplist
+const countriesInput = form.elements.countries
+const jobTierCheckboxes = form.elements['job-tier']
+const rateInput = form.elements.rate
 
-form.addEventListener("submit", e => {
-  e.preventDefault();
-  const countries = countriesInput
-                    .value
-                    .split(",")
-                    .map(country => country.trim().toLowerCase());
+form.addEventListener('submit', e => {
+  e.preventDefault()
 
-  const jobTiers = [];
+  const countries = countriesInput.value.split(',').map(
+    country => country.trim().toLowerCase()
+  )
 
-  jobTierCheckboxes.forEach(function(jobTierCheckbox) {
+  const jobTiers = []
+
+  jobTierCheckboxes.forEach(function (jobTierCheckbox) {
     if (jobTierCheckbox.checked) {
-      jobTiers.push(jobTierCheckbox.value);
+      jobTiers.push(jobTierCheckbox.value)
     }
-  });
+  })
 
-  console.log(jobTiers);
+  const rate = parseInt(rateInput.value)
 
-  chrome.storage.sync.set({ "skiplist": countries }, function () {
-    console.log("Updated list of countries to skip");
-  });
+  chrome.storage.sync.set({ skiplist: countries }, function () {
+    console.log('Updated list of countries to skip')
+  })
 
-  chrome.storage.sync.set({ "tiersToSkip": jobTiers }, function () {
-    console.log("Updated list of job tiers to skip");
-  });
+  chrome.storage.sync.set({ tiersToSkip: jobTiers }, function () {
+    console.log('Updated list of job tiers to skip')
+  })
 
-  window.location.hash = "just-updated";
-  location.reload();
-});
+  chrome.storage.sync.set({ rateToSkip: rate }, function () {
+    console.log('Updated rate below which to skip')
+  })
 
-const showSuccessMessage = function() {
+  window.location.hash = 'just-updated'
+  location.reload()
+})
+
+const showSuccessMessage = function () {
   console.log(window.location.hash)
-  if (window.location.hash === "#just-updated") {
-    const successMessage = document.createElement('p');
+  if (window.location.hash === '#just-updated') {
+    const successMessage = document.createElement('p')
 
-    successMessage.innerText     = "List updated";
-    successMessage.style.cssText = "background-color: #cdffeb; color: #07456f; padding: 2px 4px;";
+    successMessage.innerText = 'List updated'
+    successMessage.style.cssText =
+      'background-color: #cdffeb; color: #07456f; padding: 2px 4px;'
 
-    form.prepend(successMessage);
+    form.prepend(successMessage)
 
-    setTimeout(function() {
-      successMessage.remove();
-    }, 2500);
+    setTimeout(function () {
+      successMessage.remove()
+    }, 2500)
   }
 }
 
-const populateForm = function() {
-  chrome.storage.sync.get("skiplist", function(result) {
-    if (typeof result !== "undefined") {
-      console.log("Retrieved skiplist");
-      console.log(result.skiplist);
-      countriesInput.value = result.skiplist.join(", ");
+const populateForm = function () {
+  chrome.storage.sync.get('skiplist', function (result) {
+    if (typeof result !== 'undefined') {
+      console.log('Retrieved skiplist')
+      console.log(result.skiplist)
+      countriesInput.value = result.skiplist.join(', ')
     }
-  });
+  })
 
-  chrome.storage.sync.get("tiersToSkip", function(result) {
-    if (typeof result !== "undefined") {
-      console.log("Retrieved tiers to skip");
-      console.log(result.tiersToSkip);
+  chrome.storage.sync.get('tiersToSkip', function (result) {
+    if (typeof result !== 'undefined') {
+      console.log('Retrieved tiers to skip')
+      console.log(result.tiersToSkip)
 
-      result.tiersToSkip.forEach(function(tierToSkip) {
-        jobTierCheckboxes.forEach(function(checkbox) {
+      result.tiersToSkip.forEach(function (tierToSkip) {
+        jobTierCheckboxes.forEach(function (checkbox) {
           if (checkbox.value === tierToSkip) {
-            checkbox.checked = true;
+            checkbox.checked = true
           }
-        });
-      });
+        })
+      })
     }
-  });
+  })
+
+  chrome.storage.sync.get('rateToSkip', function (result) {
+    if (typeof result !== 'undefined') {
+      console.log('Retrieved rateToSkip')
+      console.log(result.rateToSkip)
+      rateInput.value = result.rateToSkip
+    }
+  })
 }
 
-window.onload = function() {
-  showSuccessMessage();
-  populateForm();
-};
+window.onload = function () {
+  showSuccessMessage()
+  populateForm()
+}
--- dist/upskip.js
+++ dist/upskip.js
@@ -1,77 +1,102 @@
-"use strict";
+/* eslint-disable no-undef */
+'use strict'
 
-let numberOfjobsSkipped = 0;
-let jobList             = null;
-let skipInProgress      = false;
+let numberOfjobsSkipped = 0
+let jobList = null
+let skipInProgress = false
 
-const skipJobs = function(jobs, countries, tiers) {
-  jobs.forEach(function(job) {
-    const countryTag = job.querySelector(".client-location");
-    const tierTag    = job.querySelector(".js-contractor-tier");
+const skipJobs = function (jobs, countries, tiers, rate) {
+  jobs.forEach(function (job) {
+    const countryTag = job.querySelector('.client-location')
+    const tierTag = job.querySelector('.js-contractor-tier')
+    const typeTag = job.querySelector('.js-type')
 
     if (countryTag !== null) {
-      const country = countryTag.innerText.toLowerCase();
+      const country = countryTag.innerText.toLowerCase()
 
-      if (countries.indexOf(country) > -1 && !job.classList.contains("upskipped")) {
-        job.classList.add("upskipped");
-        console.log(`Skipped a job from ${country}`);
-        numberOfjobsSkipped++;
-        console.log(`Number of jobs skipped: ${numberOfjobsSkipped}`);
+      if (
+        countries.indexOf(country) > -1 && !job.classList.contains('upskipped')
+      ) {
+        job.classList.add('upskipped')
+        console.log(`Skipped a job from ${country}`)
+        numberOfjobsSkipped++
+        console.log(`Number of jobs skipped: ${numberOfjobsSkipped}`)
       }
     }
 
     if (tierTag !== null) {
-      const tier = tierTag.innerText
-                   .replace('-', '')
-                   .replace(/\$/g, '')
-                   .replace('()', '')
-                   .trim()
-                   .toLowerCase();
+      const tier =
+        tierTag.innerText
+          .replace('-', '')
+          .replace(/\$/g, '')
+          .replace('()', '')
+          .trim()
+          .toLowerCase()
 
-      if (tiers.indexOf(tier) > -1 && !job.classList.contains("upskipped")) {
-        job.classList.add("upskipped");
-        console.log(`Skipped a job with tier ${tier}`);
-        numberOfjobsSkipped++;
-        console.log(`Number of jobs skipped: ${numberOfjobsSkipped}`);
+      if (tiers.indexOf(tier) > -1 && !job.classList.contains('upskipped')) {
+        job.classList.add('upskipped')
+        console.log(`Skipped a job with tier ${tier}`)
+        numberOfjobsSkipped++
+        console.log(`Number of jobs skipped: ${numberOfjobsSkipped}`)
       }
     }
-  });
+
+    if (typeTag !== null && typeTag.innerText.includes('Hourly:')) {
+      const maxRate = parseInt(typeTag.innerText.split('-$')[1])
+
+      if (rate > maxRate && !job.classList.contains('upskipped')) {
+        job.classList.add('upskipped')
+        console.log(`Skipped a job with max rate of ${maxRate}`)
+        numberOfjobsSkipped++
+        console.log(`Number of jobs skipped: ${numberOfjobsSkipped}`)
+      }
+    }
+  })
 }
 
-const findJobList = setInterval(function() {
-  console.log("Looking for job list");
-  jobList = document.body.querySelector('[data-v2-job-list]') || document.body.querySelector('#feed-jobs');
+const findJobList = setInterval(function () {
+  console.log('Looking for job list')
+
+  jobList =
+    document.body.querySelector('[data-v2-job-list]') ||
+    document.body.querySelector('#feed-jobs')
+
   if (jobList) {
-    console.log("Job list found");
-    clearInterval(findJobList);
-    jObserver.observe(jobList, jObserverConfig);
+    console.log('Job list found')
+    clearInterval(findJobList)
+    jObserver.observe(jobList, jObserverConfig)
   }
-}, 100);
+}, 100)
 
-const jObserver = new MutationObserver(function(mutations) {
+const jObserver = new MutationObserver(function (mutations) {
   if (skipInProgress === false) {
-    skipInProgress = true;
+    skipInProgress = true
 
-    setTimeout(function() {
-      const jobs      = document.body.querySelectorAll("section.job-tile");
-      let   countries = null;
-      let   tiers     = null;
+    setTimeout(function () {
+      const jobs = document.body.querySelectorAll('section.job-tile')
+      let countries = null
+      let tiers = null
+      let rate = null
 
-      chrome.storage.sync.get(['skiplist', 'tiersToSkip'], function(result) {
-        countries = result.skiplist;
-        tiers     = result.tiersToSkip;
+      chrome.storage.sync.get(
+        ['skiplist', 'tiersToSkip', 'rateToSkip'],
+        function (result) {
+          countries = result.skiplist
+          tiers = result.tiersToSkip
+          rate = result.rateToSkip
 
-        if (typeof jobs !== "undefined") {
-          skipJobs(jobs, countries, tiers);
+          if (typeof jobs !== 'undefined') {
+            skipJobs(jobs, countries, tiers, rate)
+          }
         }
-      });
+      )
 
-      skipInProgress = false;
-    }, 100);
+      skipInProgress = false
+    }, 100)
   }
-});
+})
 
 const jObserverConfig = {
   childList: true,
-  subtree:   true
-};
+  subtree: true
+}
